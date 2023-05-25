e3a117b54888a8861333ddfab0ac6ee1f21c5c18
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 09:57:13 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print('What can you do?')
+    print('| 7 | 8 | 9 | +')
+    print('| 4 | 5 | 6 | -')
+    print('| 1 | 2 | 3 | x')
+    print('| . | 0 |   | /')
+
+
+if __name__ == "__main__":
+    main()
