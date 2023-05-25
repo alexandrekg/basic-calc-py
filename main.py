bfaab140621f2ccd618dcff819d03c252ee98802
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 10:23:19 -0300

    add validation of operators [+, _, /, x]

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
 
     while True:
         expression = input('Write your expression: ')
+        operators_validator = [True if i in expression else False for i in ['+', '-', 'x', '/']]
+
+        if not any(operators_validator):
+            print('Your expression does not have an operator.')
+            return False
+
         print(expression)
         option = input('Is this result what you are expecting?  (y/n)')
         if option in ['yes', 'y', 'no', 'n']:
