cbf3ffab9f725908e9fe8d6ac93420ca0a513542
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-25 10:06:30 -0300

    creating a simple ask/response program

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ def main():
     print('| 1 | 2 | 3 | x')
     print('| . | 0 |   | /')
 
+    while True:
+        expression = input('Write your expression: ')
+        print(expression)
+        option = input('Is this result what you are expecting?  (y/n)')
+        if option in ['yes', 'y', 'no', 'n']:
+            return False
+
 
 if __name__ == "__main__":
     main()
