a12d637e4e605746ba09085e446cd77ea344789b
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2023-05-26 08:55:53 -0300

    add class, add recursive function

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-def main():
-    print('What can you do?')
-    print('| 7 | 8 | 9 | +')
-    print('| 4 | 5 | 6 | -')
-    print('| 1 | 2 | 3 | x')
-    print('| . | 0 |   | /')
 
-    while True:
-        expression = input('Write your expression: ')
-        operators_validator = [True if i in expression else False for i in ['+', '-', 'x', '/']]
+class Calculator:
+    def main(self):
+        print('What can you do?')
+        print('| 7 | 8 | 9 | +')
+        print('| 4 | 5 | 6 | -')
+        print('| 1 | 2 | 3 | x')
+        print('| . | 0 |   | /')
 
-        if not any(operators_validator):
+        self.controller()
+
+    def controller(self):
+        if getattr(self, 'result', False):
+            result_expression = input(f'What do you want to do with the result? {self.result} ')
+            self.expression = f'{self.result} {result_expression}'
+        else:
+            self.expression = input('Write your expression (Ex: n1 + n2/3 + 3):  ')
+
+        validator = self.operators_validator()
+        if not any(validator):
             print('Your expression does not have an operator.')
             return False
 
-        print(expression)
-        option = input('Is this result what you are expecting?  (y/n)')
-        if option in ['yes', 'y', 'no', 'n']:
-            return False
+        n1, operator, n2 = self.expression.split(' ')
+        self.calc(int(n1), int(n2), operator)
+        print(f'Result of {self.expression} is: {self.result}')
+
+        self.controller()
+
+    def calc(self, n1, n2, operator):
+        if operator == '/':
+            self.result = int(n1 / n2)
+        elif operator == '+':
+            self.result = int(n1 + n2)
+        elif operator == '-':
+            self.result = int(n1 - n2)
+        elif operator in ['*', 'x']:
+            self.result = int(n1 * n2)
+
+    def operators_validator(self):
+        return [True if i in self.expression else False for i in ['+', '-', 'x', '/']]
 
 
 if __name__ == "__main__":
-    main()
+    Calculator().main()
